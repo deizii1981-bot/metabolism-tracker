98400fdecd5758cc5ba8f21b78e33c402dae3ea7
Anjali Patil <20079200@dbs.ie>
2025-12-11 07:52:22 +0530

    Cleanup: remove stray file frontend/cd and restore backend/db.json

---
+++ backend/package.js
@@ -0,0 +1,21 @@
+{
+  "name": "backend",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "jest"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "type": "commonjs",
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^5.1.0"
+  },
+  "devDependencies": {
+    "jest": "^30.2.0",
+    "node-fetch": "^2.7.0"
+  }
+}
